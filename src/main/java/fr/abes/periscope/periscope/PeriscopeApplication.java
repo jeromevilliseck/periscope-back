b9c6755ab7902492b9e08bfe87322ecc21f778ca
Jerome Villiseck <jerome.villiseck@gmail.com>
2021-01-06 14:43:14 +0100

    firest commit

---
+++ src/main/java/fr/abes/periscope/periscope/PeriscopeApplication.java
@@ -0,0 +1,13 @@
+package fr.abes.periscope.periscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeriscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeriscopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/abes/periscope/periscope/PeriscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abes.periscope.periscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c84eb186a39ab4702e3e9e6c36db8fdf469fdf8c
Angot Thomas <thomas.angot205@gmail.com>
2021-01-08 16:15:53 +0100

    creation of springboot project

---
+++ src/main/java/fr/abes/periscope/periscope/SpringbootResterverApplication.java
@@ -0,0 +1,33 @@
+package fr.abes.periscope.periscope;
+
+package com.bnguimgo.springbootrestserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+
+public class SpringbootRestserverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestserverApplication.class, args);
+    }
+}
+
+@Controller
+public class RestServices {
+    private static final Logger logger = LoggerFactory.getLogger(RestServices.class);
+
+    @GetMapping(value = "/")
+    public ResponseEntity<String> pong() {
+        logger.info("Démarrage des services OK .....");
+        return new ResponseEntity<String>("Réponse du serveur: " + HttpStatus.OK.name(),
+                HttpStatus.OK);
+    }
+}
